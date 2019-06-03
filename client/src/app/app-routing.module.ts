c41f69de92adc995ee153faa2f6ada6add1546e1
chloe.prudham <chloe.prudham@uha.fr>
2019-06-03 10:08:05 +0200

    profile transport

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuthGuard} from './guards/auth.guard';
+import {NoAuthGuard} from './guards/no-auth.guard';
 
-const routes: Routes = [];
+// guest
+import { LoginComponent } from "./guest/login/login.component";
+import { RegisterComponent } from "./guest/register/register.component";
+// adm
+import {AdmComponent} from "./adm/adm/adm.component";
+// user
+
+// transporteur
+import {ChaufferComponent} from "./transporteur/chauffer/chauffer.component";
+import {VoituresComponent} from "./transporteur/voitures/voitures.component";
+import {TransporteurProfileComponent} from "./transporteur/transporteur-profile/transporteur-profile.component";
+import {HomeComponent} from "./home/home.component";
+
+
+
+const routes: Routes = [
+  // other
+  { path: '', component: HomeComponent, canActivate: [NoAuthGuard]},
+  // adm
+  { path: 'adm', component: AdmComponent, canActivate: [NoAuthGuard]},
+  // guest
+  { path: 'login', component: LoginComponent, canActivate: [NoAuthGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [NoAuthGuard] },
+  // user
+  // transporteur
+  { path: 'chauffer', component: ChaufferComponent, canActivate: [NoAuthGuard] },
+  { path: 'voitures', component: VoituresComponent, canActivate: [NoAuthGuard] },
+  { path: 'transporteur', component: TransporteurProfileComponent, canActivate: [NoAuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,18 +4,55 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthGuard } from "./guards/auth.guard";
-import {HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+// guest
+import { LoginComponent } from "./guest/login/login.component";
+import { RegisterComponent } from "./guest/register/register.component";
+
+// admin
+import { AdmComponent } from "./adm/adm/adm.component";
+
+// user
+import { UserProfileComponent } from './user/user-profile/user-profile.component';
+
+// transporteur
+import { ChaufferComponent } from "./transporteur/chauffer/chauffer.component";
+import { VoituresComponent } from "./transporteur/voitures/voitures.component";
+import { TransporteurProfileComponent } from './transporteur/transporteur-profile/transporteur-profile.component';
+
+// services
+import { ExpressService } from "./services/express.service";
+import { LoginService } from "./services/login.service";
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    AdmComponent,
+    UserProfileComponent,
+    ChaufferComponent,
+    VoituresComponent,
+    TransporteurProfileComponent,
+    HomeComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [AuthGuard],
+  providers: [
+    AuthGuard,
+    ExpressService,
+    LoginService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
